f5878eac56d9198793232dcdec1abddb631dd3c9
Alex Gaming Studios <team.alexgaming@icloud.com>
2025-12-26 11:17:19 +0100

    Add Form1.Designer.cs for Amazon desktop app

---
+++ project_files/Form1.Designer.cs
@@ -0,0 +1,27 @@
+namespace Amazon
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1400, 900);
+            this.Name = "Form1";
+            this.Text = "Amazon Shopping for Desktop";
+            this.ResumeLayout(false);
+        }
+    }
+}
